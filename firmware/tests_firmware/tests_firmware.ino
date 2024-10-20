9e29790f58d71a8c3e153aed68545eb32871e359
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2024-10-20 13:02:11 +0100

    rfid mods

--- firmware/tests_firmware/tests_firmware.ino
+++ firmware/tests_firmware/tests_firmware.ino
@@ -6,6 +6,9 @@
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
+byte smallcup[7] = {4,197,86,97,16,2,137}; 
+byte bigcup[7] = {4,127,179,222,16,1,136};
+
 void setup() {
   Serial.begin(9600);
   SPI.begin();
@@ -27,14 +30,42 @@ void loop() {
 
   // visualizza l'UID sulla porta seriale di Arduino IDE
   //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
-  printHex(mfrc522.uid.uidByte, mfrc522.uid.size);
+
+  printUID(mfrc522.uid.uidByte, mfrc522.uid.size);
+  bool result = compareUID(mfrc522.uid.uidByte, mfrc522.uid.size);
+  Serial.println(result);
   mfrc522.PICC_HaltA();
 }
 
-void printHex (byte* buffer, byte buffersize){
-  for (byte i = 0; i < buffersize; i++) {
-  //Serial.println(buffer[i]);
-  Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-  Serial.print(buffer[i], HEX);
+bool compareUID(byte* buffer, byte buffersize){
+  if(buffersize != 7){
+    Serial.println("Error! UID with differnt size");
+    return;
+  }
+
+  bool exists1 = true;
+  for(int i = 0; i<buffersize; i++){
+    if(buffer[i] != smallcup[i]){
+      exists1 = false;
+      break;
+    }
+  }
+  
+  bool exists2 = true;
+  for(int i = 0; i<buffersize; i++){
+    if(buffer[i] != bigcup[i]){
+      exists2 = false;
+      break;
+    }
+  }
+
+  return exists1 || exists2;
+}
+
+void printUID(byte* buffer, byte buffersize){
+  for(int i = 0; i<buffersize; i++){
+    Serial.print(i);
+    Serial.print(" : ");
+    Serial.println(buffer[i]);
   }
 }
