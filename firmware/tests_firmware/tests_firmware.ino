b78e14c2c5b0e9706cf15be29f01f279d02540ee
Luca <luca.dipietro@mail.polimi.it>
2024-10-20 13:34:24 +0100

    led RFID

--- firmware/tests_firmware/tests_firmware.ino
+++ firmware/tests_firmware/tests_firmware.ino
@@ -3,11 +3,12 @@
 
 #define RST_PIN  9 // RES pin
 #define SS_PIN  10 // SDA (SS) pin
+#define LED_PIN 7 
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
-byte smallcup[7] = {4,197,86,97,16,2,137}; 
-byte bigcup[7] = {4,127,179,222,16,1,136};
+byte smallcup[7] = {4,197,86,97,16,2,136}; 
+byte bigcup[7] = {4,127,179,222,16,1,137};
 
 void setup() {
   Serial.begin(9600);
@@ -16,6 +17,9 @@ void setup() {
   delay(4);
   mfrc522.PCD_DumpVersionToSerial();
   Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
 }
 
 void loop() {
@@ -30,11 +34,18 @@ void loop() {
 
   // visualizza l'UID sulla porta seriale di Arduino IDE
   //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+  MFRC522::PICC_Type PICC_Type = mfrc522.PICC_GetType(mfrc522.uid.sak);
 
   printUID(mfrc522.uid.uidByte, mfrc522.uid.size);
   bool result = compareUID(mfrc522.uid.uidByte, mfrc522.uid.size);
   Serial.println(result);
+  if (result == 1){
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
   mfrc522.PICC_HaltA();
+
 }
 
 bool compareUID(byte* buffer, byte buffersize){
@@ -60,6 +71,7 @@ bool compareUID(byte* buffer, byte buffersize){
   }
 
   return exists1 || exists2;
+
 }
 
 void printUID(byte* buffer, byte buffersize){
