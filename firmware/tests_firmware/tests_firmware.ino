e109c197ced42ca3beee033e94928644ee3427e2
Luca <luca.dipietro@mail.polimi.it>
2024-11-09 20:07:23 +0000

    Somw valve updates

--- firmware/tests_firmware/tests_firmware.ino
+++ firmware/tests_firmware/tests_firmware.ino
@@ -1,13 +1,13 @@
 #include "SPI.h"
 #include "MFRC522.h"
 #include "Vector.h"
-#include <string.h> 
+#include <string.h>
 
-#define RST_PIN  9 // RES pin
-#define SS_PIN  10 // SDA (SS) pin
+#define RST_PIN 9  // RES pin
+#define SS_PIN 10  // SDA (SS) pin
 #define LED_PIN 6
-#define VALVE_PIN 3
-#define FLOW_PIN 8
+#define VALVE_PIN 8
+#define FLOW_PIN 2
 
 struct UID {
   String name;
@@ -21,16 +21,24 @@ bool isPouring = false;
 bool pouringEnded = true;
 
 //FLUX SENSOR
+
+volatile int pulseCount;
+float flowRate;
+unsigned long flowMilliLitres;
+unsigned long totalMilliLitres;
+unsigned long oldTime;
+
+/*
 unsigned int flow_count = 0;
 unsigned int prev_count = 0;
 unsigned long prev_time = millis();
 bool b_wheel_turning = false;
-float volume_per_pulse = 2.25; //volume in ml per pulse (sensor YF-S201)
-float total_volume = 0.0; 
+float volume_per_pulse = 2.25;  //volume in ml per pulse (sensor YF-S201)
+float total_volume = 0.0;
 
 void setup() {
   Serial.begin(9600);
-  
+
   //RFID
   SPI.begin();
   mfrc522.PCD_Init();
@@ -42,8 +50,8 @@ void setup() {
   digitalWrite(LED_PIN, LOW);
 
   //TEST POPULATION
-  byte sbyte[7] = {4,197,86,97,16,2,137};
-  byte bbyte[7] = {4,127,179,222,16,1,137};
+  byte sbyte[7] = { 4, 197, 86, 97, 16, 2, 137 };
+  byte bbyte[7] = { 4, 127, 179, 222, 16, 1, 137 };
   UID small;
   UID big;
   small.name = "smallcup";
@@ -55,7 +63,7 @@ void setup() {
 
   //FLUX SENSOR
   pinMode(FLOW_PIN, INPUT_PULLUP);
-  attachInterrupt( FLOW_PIN, FlowCounter, FALLING );
+  attachInterrupt(digitalPinToInterrupt(FLOW_PIN), FlowCounter, FALLING);
   isPouring = false;
   pouringEnded = true;
 
@@ -67,13 +75,13 @@ void setup() {
 void loop() {
 
 
-  if(!isPouring){
+  if (!isPouring) {
     // reset del ciclo quando nessuna scheda è inserita nel lettore
-    if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    if (!mfrc522.PICC_IsNewCardPresent()) {
       return;
     }
 
-    if ( ! mfrc522.PICC_ReadCardSerial()) {
+    if (!mfrc522.PICC_ReadCardSerial()) {
       return;
     }
 
@@ -82,10 +90,10 @@ void loop() {
     MFRC522::PICC_Type PICC_Type = mfrc522.PICC_GetType(mfrc522.uid.sak);
 
     PrintUID(mfrc522.uid.uidByte, mfrc522.uid.size);
-    bool existsUID  = true;//= CompareUID(mfrc522.uid.uidByte, mfrc522.uid.size);
+    bool existsUID = true;  //= CompareUID(mfrc522.uid.uidByte, mfrc522.uid.size);
     //byte buffer[10];
     //ReadDataBlock(0, buffer, 10);
-    if(existsUID){
+    if (existsUID) {
       digitalWrite(LED_PIN, HIGH);
       digitalWrite(VALVE_PIN, HIGH);
       Serial.println("VALVE ACTIVATED");
@@ -95,11 +103,11 @@ void loop() {
     mfrc522.PICC_HaltA();
   }
 
-  if(isPouring){
+  if (isPouring) {
     PouringRoutine();
   }
 
-  if(pouringEnded){
+  if (pouringEnded) {
     Serial.println("VALVE CLOSED");
     digitalWrite(LED_PIN, LOW);
     digitalWrite(VALVE_PIN, LOW);
@@ -118,11 +126,11 @@ void ReadDataBlock(int block, byte* buffer, int buffersize){
   Serial.println("Block was read!");
 }
 */
-void PouringRoutine(){
-  if(pouringEnded){
+void PouringRoutine() {
+  if (pouringEnded) {
     total_volume = 0;
     flow_count = 0;
-    prev_count = 0; // Inizializza prev_count
+    prev_count = 0;  // Inizializza prev_count
     pouringEnded = false;
   }
 
@@ -144,54 +152,54 @@ void PouringRoutine(){
     Serial.println(total_volume);
     Serial.println("");
   }
-  if(total_volume > 200){
+  if (total_volume > 200) {
     pouringEnded = true;
   }
 }
 
 
-bool CompareUID(byte* buffer, byte buffersize){
-  if(buffersize != 7){
+bool CompareUID(byte* buffer, byte buffersize) {
+  if (buffersize != 7) {
     Serial.println("Error! UID with differnt size");
     return 0;
   }
 
-  String result= "none";
-  for(int j = 0; j<UIDs.size(); j++){
+  String result = "none";
+  for (int j = 0; j < UIDs.size(); j++) {
     bool found = true;
-    for(int i = 0; i<buffersize; i++){
+    for (int i = 0; i < buffersize; i++) {
       Serial.print(buffer[i]);
       Serial.print("-");
       Serial.print(UIDs[j].uid[i]);
       Serial.print(":");
-      if(buffer[i] != UIDs[j].uid[i]){
+      if (buffer[i] != UIDs[j].uid[i]) {
         Serial.println("not equal");
         found = false;
         break;
-      }  
+      }
     }
-    if(found){
+    if (found) {
       result = UIDs[j].name;
       break;
     }
   }
 
-  if(result == "none"){
+  if (result == "none") {
     return false;
   } else {
     return true;
   }
 }
 
-void PrintBuffer(byte* buffer, byte buffersize){
-  for(int i = 0; i<buffersize; i++){
+void PrintBuffer(byte* buffer, byte buffersize) {
+  for (int i = 0; i < buffersize; i++) {
     Serial.print(buffer[i]);
   }
   Serial.println();
 }
 
-void PrintUID(byte* buffer, byte buffersize){
-  for(int i = 0; i<buffersize; i++){
+void PrintUID(byte* buffer, byte buffersize) {
+  for (int i = 0; i < buffersize; i++) {
     Serial.print(buffer[i]);
     Serial.print(" ");
   }
@@ -199,6 +207,6 @@ void PrintUID(byte* buffer, byte buffersize){
 }
 
 //FLUX SENSOR
-void FlowCounter(){
+void FlowCounter() {
   flow_count++;
 }
