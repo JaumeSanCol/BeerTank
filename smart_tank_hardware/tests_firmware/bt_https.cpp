b861c4c44bf07a4889f154f8070d5b48f67a9c9e
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2025-01-04 18:28:19 +0100

    WaitingConfirmation

--- smart_tank_hardware/tests_firmware/bt_https.cpp
+++ smart_tank_hardware/tests_firmware/bt_https.cpp
@@ -12,11 +12,10 @@ int validateToken(WiFiClientSecure& client, int tokenId) {
   int code = 500;
 
   client.setInsecure();
-  String url = "/verify/token/" + String(tokenId);
+  String url = "/verify/token/" + String(tokenId); // I need to change that
 
   //Serial.print("Requesting URL: ");
-  //Serial.println(url)
-  ;
+  //Serial.println(url);
   client.stop();
   if (client.connect(server, 443)) {
     // Serial.println(F("Connected to server successfully"));
@@ -27,7 +26,6 @@ int validateToken(WiFiClientSecure& client, int tokenId) {
     client.println();
 
     // Serial.println(F("Headers sent to server successfully"));
-
     while (client.connected()) {
       String line = client.readStringUntil('\n');
       if (line == "\r") {
@@ -41,9 +39,18 @@ int validateToken(WiFiClientSecure& client, int tokenId) {
 
     if (start != -1 && end != -1 && end > start) {
       String result = response.substring(start + 1, end);
-      if (result == "true") code = 200;
-      else if (result == "false") code = 404;
-      else Serial.println("Unexpected response");
+      if (result == "true"){
+        code = 200;
+
+        WaitingConfirmation();
+        
+      } 
+      else if (result == "false"){
+        code = 404;
+      } 
+      else{
+        Serial.println("Unexpected response");
+      }
 
     } else Serial.println("Unexpected response");
   } else {
@@ -53,3 +60,56 @@ int validateToken(WiFiClientSecure& client, int tokenId) {
   client.stop();
   return code;
 }
+
+
+void WaitConfirmation(){
+  int maxTime = 30;
+  bool isConfirmed = false;
+
+  While(!isConfirmed){
+    if(maxTime <= 0){
+      break;
+    }
+
+    String url = "/verify/token/" + String(tokenId); // I need to change that
+
+    //Serial.print("Requesting URL: ");
+    //Serial.println(url);
+    client.stop();
+    if (client.connect(server, 443)) {
+      // Serial.println(F("Connected to server successfully"));
+      client.println("POST " + url + " HTTP/1.1");
+      client.println("Host: " + String(server));
+      client.println("special-key: arduinoUser");
+      client.println("Content-Type: application/json");
+      client.println();
+
+      // Serial.println(F("Headers sent to server successfully"));
+      while (client.connected()) {
+        String line = client.readStringUntil('\n');
+        if (line == "\r") {
+          break;
+        }
+      }
+
+      String response = client.readString();
+      int start = response.indexOf(':');
+      int end = response.indexOf(',');
+
+      if (start != -1 && end != -1 && end > start) {
+        String result = response.substring(start + 1, end);
+        Serial.println(result);
+        result = "true";
+        if(result == "true"){
+          isConfirmed = true;
+        }
+      }
+    }
+    delay(5000);
+    maxTime -= 5;
+  }
+}
+
+
+
+
